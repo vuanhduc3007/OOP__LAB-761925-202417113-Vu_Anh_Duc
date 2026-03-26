ee23acc43c8313055e65b17abc8a6b2ce43a1111
Vu Anh Duc <vuanhduc3007@gmail.com>
2026-03-26 13:47:21 +0700

    Complete assignment 2.2.3

--- Lab01/FirstDialog.java
+++ Lab01/FirstDialog.java
@@ -1,5 +1,7 @@
 package Lab01;
+
 import javax.swing.*;
+
 public class FirstDialog {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hello World! how are you?", null, 0);
---
+++ Lab01/HelloNameDialog.java
@@ -0,0 +1,10 @@
+package Lab01;
+import javax.swing.JOptionPane;
+public class HelloNameDialog {
+    public static void main(String[] args) {
+        String result;
+        result = JOptionPane.showInputDialog(null, "Please enter your name");
+        JOptionPane.showMessageDialog(null,"Hi "+result+"!");
+        System.exit(0);
+    }
+}
