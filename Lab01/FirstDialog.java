638d6ab31466be9f9c14a2512495cee211ead2de
Vu Anh Duc <vuanhduc3007@gmail.com>
2026-03-26 13:40:25 +0700

    Complete assignment 2.2.2

--- Lab01/FirstDialog.java
+++ Lab01/FirstDialog.java
@@ -1,5 +1,8 @@
 package Lab01;
-
+import javax.swing.*;
 public class FirstDialog {
-    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World! how are you?", null, 0);
+        System.exit(0);
+    }
 }
