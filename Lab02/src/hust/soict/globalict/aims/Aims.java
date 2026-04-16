6985b87d059fe70d97c5fb1207efecef4f3a0002
user.email <khanhluongbao@gmail.com>
2026-04-16 14:54:19 +0700

    Implement Playable interface for Track, DVD and CD

--- Lab02/src/hust/soict/globalict/aims/Aims.java
+++ Lab02/src/hust/soict/globalict/aims/Aims.java
@@ -1,5 +1,5 @@
 package hust.soict.globalict.aims;
-import hust.soict.globalict.aims.disc.DigitalVideoDisc;
+import hust.soict.globalict.aims.media.DigitalVideoDisc;
 import hust.soict.globalict.aims.cart.Cart;;
 
 public class Aims {
@@ -7,22 +7,20 @@ public class Aims {
         Cart anOrder = new Cart();
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addMedia(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addMedia(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        anOrder.addDigitalVideoDisc(dvd3);
+        anOrder.addMedia(dvd3);
 
         System.out.println("Total Cost is: ");
         System.out.println(anOrder.totalCost());
 
         System.out.println("\nRemoving Star Wars...");
-        anOrder.removeDigitalVideoDisc(dvd2);
+        anOrder.removeMedia(dvd2);
         System.out.println("Total Cost after removing is: " + anOrder.totalCost());
 
-        DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3};
-        anOrder.addDigitalVideoDisc(dvdList);
     }
 }
